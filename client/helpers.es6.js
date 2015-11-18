2dc0d84f1b24809c6ecc1c05adcdb7a065ce29d9
Wat Chun Pang Gilbert <gilbert.wat@getthingsdone.hk>
2015-11-18 11:24:29 +0800

    Put dashboard specific logic back

--- client/helpers.es6.js
+++ client/helpers.es6.js
@@ -51,25 +51,6 @@ Template.registerHelper("formatDurationPreciseMs", DurationFormatter.toPreciseMs
 
 Template.registerHelper("formatAmount", AmountFormatter.toString);
 
-Template.registerHelper('enableIfIsEditing', function(isEditing) {
-  return isEditing.get() ? "false" : "true";
-});
-
-Template.registerHelper("formatMessage", function(text) {
-  text = Blaze.toHTMLWithData(UI._globalHelpers['emojione'], text);
-  text = UI._globalHelpers['nl2br'](text);
-  return text;
-});
-
 Template.registerHelper('not', function(x) {
   return !x;
-});
-
-Template.registerHelper('isAdmin', function() {
-  return Meteor.userId() && Users.isAdmin(Meteor.userId());
-})
-
-Template.registerHelper('getSelectChannelData', function () {
-  let customer = Template.currentData().currentCustomer;
-  return { _id : customer._id };
 });
